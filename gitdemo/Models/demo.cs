009b641b73ff9efe683c1fe500f7ebcded3bcf7b
Dakshita <Dakshitha@capg.com>
2026-01-15 15:23:53 +0530

    Welcome home

---
+++ gitdemo/Models/demo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gitdemo.Models
+{
+    public class demo : Controller
+    {
+        public IActionResult Index()
+        {
+            // hi, how you doing?
+            return View();
+        }
+    }
+}
